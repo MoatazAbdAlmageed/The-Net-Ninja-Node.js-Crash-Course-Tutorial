64cf715a4ab71eb6f1cf603f14178412eb700f60
MoatazAbdAlmageed <moataz@wuilt.com>
2020-07-02 17:23:42 +0200

    show

--- server.ts
+++ server.ts
@@ -1,14 +1,19 @@
 const express = require("express");
 const ejs = require("ejs");
 const app = express();
-
+const blogs = [
+  { title: "this is title 1", content: "this is content 1" },
+  { title: "this is title 2", content: "this is content 2" },
+  { title: "this is title 3", content: "this is content 3" },
+  { title: "this is title 4", content: "this is content 4" },
+];
 app.set("view engine", "ejs");
 
 app.listen(3000);
 
 app.get("/", (req, res) => {
   // res.sendFile(`views/index.html`, { root: __dirname });
-  res.render("index", { title: "Home" });
+  res.render("index", { title: "Home", blogs });
 });
 app.get("/about", (req, res) => {
   // res.sendFile(`views/about.html`, { root: __dirname });
