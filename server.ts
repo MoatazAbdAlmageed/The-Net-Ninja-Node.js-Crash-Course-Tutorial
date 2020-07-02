9bce4dd7987c9ac649ecd715f019c673916b5696
MoatazAbdAlmageed <moataz@wuilt.com>
2020-07-02 17:13:01 +0200

    pass

--- server.ts
+++ server.ts
@@ -8,17 +8,32 @@ app.listen(3000);
 
 app.get("/", (req, res) => {
   // res.sendFile(`views/index.html`, { root: __dirname });
-  res.render("index");
+  res.render("index", { title: "Home" });
 });
 app.get("/about", (req, res) => {
   // res.sendFile(`views/about.html`, { root: __dirname });
-  res.render("about");
+  res.render("about", { title: "About" });
+});
+app.get("/services", (req, res) => {
+  // res.sendFile(`views/services.html`, { root: __dirname });
+  res.render("services", { title: "Services" });
 });
 app.get("/about-me", (req, res) => {
   res.redirect("/about");
 });
 
+app.get("/blogs/create", (req, res) => {
+  // pass data to view
+  res.render("create", { title: "Create Post" });
+});
+app.post("/create", (req, res) => {
+  console.log("🚀🚀🚀🚀🚀🚀 req");
+  console.log(req);
+  console.log("----------------------------------------------------");
+  console.log();
+});
+
 app.use((req, res) => {
   // res.sendFile(`views/404.html`, { root: __dirname });
-  res.render("404");
+  res.render("404", { title: "404" });
 });
