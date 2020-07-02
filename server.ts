6a3023b3872475ebe779c10d1810cb8785a14ad4
MoatazAbdAlmageed <moataz@wuilt.com>
2020-07-02 16:49:30 +0200

    use

--- server.ts
+++ server.ts
@@ -1,17 +1,24 @@
 const express = require("express");
+const ejs = require("ejs");
 const app = express();
+
+app.set("view engine", "ejs");
+
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.sendFile(`views/index.html`, { root: __dirname });
+  // res.sendFile(`views/index.html`, { root: __dirname });
+  res.render("index");
 });
 app.get("/about", (req, res) => {
-  res.sendFile(`views/about.html`, { root: __dirname });
+  // res.sendFile(`views/about.html`, { root: __dirname });
+  res.render("about");
 });
 app.get("/about-me", (req, res) => {
   res.redirect("/about");
 });
 
 app.use((req, res) => {
-  res.sendFile(`views/404.html`, { root: __dirname });
+  // res.sendFile(`views/404.html`, { root: __dirname });
+  res.render("404");
 });
