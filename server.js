756f606dc7264d5519dadb6ef6654fe52636dd7c
Moataz Mohammady <moataz@wuilt.com>
2020-07-03 15:03:05 +0200

    create task

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const app = express();
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const Task = require("./models/task");
+var bodyParser = require("body-parser");
+
+// for parsing application/json
+// app.use(bodyParser.json());
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
+//form-urlencoded
 mongoose.connect("mongodb://localhost/board", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -35,7 +43,7 @@ app.get("/", (req, res) => {
 });
 app.get("/show/:id", (req, res) => {
   tasks = Task.findById(`${req.params.id}`).then((task) => {
-    res.render("show", { title: "Task", task });
+    res.render("show", { title: task.title, task });
   });
 });
 app.get("/about", (req, res) => {
@@ -52,12 +60,9 @@ app.get("/create", (req, res) => {
   res.render("create", { title: "Create" });
 });
 app.post("/create", (req, res) => {
-  const task = new Task({ title: "test", content: "this is  content" });
+  const { title, content } = req.body;
+  const task = new Task({ title, content });
   task.save().then(() => {
-    console.log("task created");
-    console.log("🚀🚀🚀🚀🚀🚀 req");
-    console.log("---------------------- saving --------------------------");
-    console.log();
     res.redirect("/");
   });
 });
